be7af55fcbb0ea1622d1f7f111061e1ae459c074
SISUBEN <100661067+SISUBEN@users.noreply.github.com>
2023-03-08 18:19:38 +0800

    Add files via upload

---
+++ keyboard.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <Windows.h>
+using namespace std;
+
+int main()
+{
+    
+    // 注册两个热键 Ctrl+F1 , Ctrl+F2
+    if (0 == RegisterHotKey(NULL, 1, MOD_CONTROL, VK_F2)) {
+        cout << "RegisterHotKey error : " << GetLastError() << endl;
+    }
+    // if (0 == RegisterHotKey(NULL, 2, MOD_CONTROL, VK_F2)) {
+    //     cout << "RegisterHotKey error : " << GetLastError() << endl;
+    // }
+
+    // 消息循环
+    MSG msg = { 0 };
+    while (GetMessage(&msg, NULL, 0, 0)){
+
+        switch (msg.message) {
+        case WM_HOTKEY:
+        {
+            if (1 == msg.wParam) {
+				POINT p;
+				GetCursorPos(&p);
+				mouse_event(MOUSEEVENTF_ABSOLUTE | MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0);
+				mouse_event(MOUSEEVENTF_ABSOLUTE | MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
+            }
+                
+            // else if (2 == msg.wParam) {
+            //     cout << "2" << endl;
+            // }
+
+            break;
+        }
+            
+        default:
+            break;
+        }
+
+    }
+
+    cout << "finished." << endl;
+    return 0;
+}
